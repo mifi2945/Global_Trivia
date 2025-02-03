378abaf4a82b1641984dcea89dfa699bb5d5f13e
Mikhail Filippov <mifi2945@gmail.com>
2025-02-02 23:23:09 -0600

    Class file created, file structure made

---
+++ src/main/java/filippovm/APITest.java
@@ -0,0 +1,13 @@
+/**
+ * Mikhail Filippov
+ * 02/02/2025
+ * PLaying around with Maven and Java APIs
+ */
+
+package filippovm;
+
+public class APITest {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
